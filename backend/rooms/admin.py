2533b5f237fd6b80d2ffb6e9a8c44ec570d72d02
Simon Egorov <rhinoRofl@gmail.com>
2022-12-06 21:33:37 +0600

    Create rooms admin

--- backend/rooms/admin.py
+++ backend/rooms/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from rooms.models import Room, Booking
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'number',
+        'name',
+        'price',
+        'capacity',
+    )
+    list_editable = (
+        'number',
+        'name',
+        'price',
+        'capacity',
+    )
+    search_fields = ('name', 'number', 'capacity',)
+    list_filter = ('price', 'capacity',)
+    empty_value_display = 'empty'
+
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'room',
+        'guest',
+        'date_range',
+    )
+    list_editable = (
+        'room',
+        'guest',
+        'date_range',
+    )
+    search_fields = ('room', 'guest', 'date_range',)
+    list_filter = ('room', 'guest', 'date_range',)
+    empty_value_display = 'empty'
+
+
+# class IngredientInline(admin.TabularInline):
+#
+#     model = IngredientQuantity
+#     extra = 1
+
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Booking, BookingAdmin)
