65a28da825799ca5032afb3fadd3bb0cdd4a97f3
Simon Egorov <rhinoRofl@gmail.com>
2022-12-06 21:35:37 +0600

    Create api urls

---
+++ backend/api/urls.py
@@ -0,0 +1,23 @@
+from django.urls import include, path
+# from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
+#                                    SpectacularSwaggerView)
+# from rest_framework.authtoken import views
+from rest_framework.routers import SimpleRouter
+
+from api.users_views import UserViewSet
+
+app_name = 'api'
+
+router = SimpleRouter()
+
+router.register('users', UserViewSet, basename='user')
+# router.register('rooms', RoomViewSet, basename='room')
+
+
+urlpatterns = [
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router.urls)),
+    # path('v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # path('v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # path('v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+]
