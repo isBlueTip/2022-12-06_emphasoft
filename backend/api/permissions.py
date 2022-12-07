1784453163b61ea3849f666999b8e5878316e2b5
Simon Egorov <rhinoRofl@gmail.com>
2022-12-07 17:04:25 +0600

    Update permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,7 +1,5 @@
 from rest_framework.permissions import (SAFE_METHODS, BasePermission,
-                                        IsAuthenticatedOrReadOnly)
-
-# from users.models import ADMIN
+                                        IsAdminUser)
 
 
 class IsAuthenticatedOrReadOnlyOrRegister(BasePermission):
@@ -14,10 +12,28 @@ class IsAuthenticatedOrReadOnlyOrRegister(BasePermission):
         )
 
 
-class IsAdminOrIsAuthorOrReadOnly(IsAuthenticatedOrReadOnly):
-    def has_object_permission(self, request, view, obj):
+class IsAdminOrCreate(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            (request.user and request.user.is_authenticated)
+        )
+
+
+# class IsAuthor(BasePermission):
+#     def has_permission(self, request, view):
+#         print(1)
+#         return bool(
+#             # request.method == SAFE_METHODS
+#             request.user and request.user.is_authenticated
+#         )
+#
+#     def has_object_permission(self, request, view, obj):
+#         return bool(obj.guest == request.user)
+
+
+class IsAdminOrReadOnly(IsAdminUser):
+    def has_permission(self, request, view):
         return bool(
             request.method in SAFE_METHODS
-            or obj.author == request.user
-            # or request.user.role == ADMIN
+            or (request.user and request.user.is_staff)
         )
