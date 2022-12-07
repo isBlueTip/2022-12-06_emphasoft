bb0bbf86c4d136706f753a3840aff9da1b79e68a
Simon Egorov <rhinoRofl@gmail.com>
2022-12-07 17:07:48 +0600

    Update room views

--- backend/api/rooms_views.py
+++ backend/api/rooms_views.py
@@ -1,25 +1,48 @@
-from rest_framework import mixins, status, viewsets
-from rest_framework.decorators import action
-from rest_framework.permissions import (AllowAny, BasePermission,
-                                        IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
-from rest_framework.response import Response
+import logging
 
-from api.permissions import IsAuthor
-from api.rooms_serializers import RoomSerializer, BookingSerializer
-from rooms.models import Room, Booking
+from rest_framework import viewsets
+
+from api.permissions import IsAdminOrCreate, IsAdminOrReadOnly  # , IsAuthor
+from api.rooms_serializers import BookingSerializer, RoomSerializer
+from rooms.models import Booking, Room
+
+logger = logging.getLogger('logger')
 
 
-class RoomViewSet(viewsets.ReadOnlyModelViewSet):
+class RoomViewSet(viewsets.ModelViewSet):
 
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class BookingViewSet(viewsets.ModelViewSet):
 
-    queryset = Booking.objects.all()
-    pagination_class = None
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Booking.objects.all()
+        return Booking.objects.filter(guest=user)
+
+    pagination_class = None  # TODO which pagination?
     serializer_class = BookingSerializer
-    permission_classes = [IsAuthor]
+    permission_classes = [IsAdminOrCreate]
+
+    # @action(
+    #     detail=False,
+    #     methods=[
+    #         "get",
+    #         "delete",
+    #     ],
+    #     serializer_class=BookingSerializer,
+    #     permission_classes=[IsAuthor,]
+    # )
+    # def my_bookings(self, request,*args, **kwargs):
+    #     queryset = self.get_queryset()
+    #     page = self.paginate_queryset(queryset)
+    #     if page is not None:
+    #         serializer = self.get_serializer(page, many=True)
+    #         return self.get_paginated_response(serializer.data)
+    #
+    #     serializer = self.get_serializer(queryset, many=True)
+    #     return Response(serializer.data)
