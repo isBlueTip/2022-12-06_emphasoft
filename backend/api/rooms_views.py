6e63769b7f09dcbd411fab3e969d403ea6a7d974
Simon Egorov <rhinoRofl@gmail.com>
2022-12-08 13:47:45 +0600

    Add available rooms search

--- backend/api/rooms_views.py
+++ backend/api/rooms_views.py
@@ -1,9 +1,12 @@
 import logging
+from http import HTTPStatus
 
-from rest_framework import viewsets
-
+from api.filters import RoomFilter
 from api.permissions import IsAdminOrCreate, IsAdminOrReadOnly  # , IsAuthor
 from api.rooms_serializers import BookingSerializer, RoomSerializer
+from django.db.models import Q
+from rest_framework import viewsets
+from rest_framework.response import Response
 from rooms.models import Booking, Room
 
 logger = logging.getLogger('logger')
@@ -14,10 +17,52 @@ class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filterset_class = RoomFilter
+    lookup_url_kwargs = ['checkin_date', 'checkout_date']
+
+    def validate(self):
+        checkin_date = self.request.query_params.get('checkin_date')
+        logger.debug(checkin_date)
+        logger.debug(checkin_date)
+        pass
+
+    def list(self, request, *args, **kwargs):
+        checkin_date = request.query_params.get('checkin_date')
+        checkout_date = request.query_params.get('checkout_date')
+        if checkin_date and checkout_date:
+            # logger.debug(checkin_date)
+            queryset = Room.objects.exclude(
+                Q(
+                    bookings__date_check_in__lte=checkin_date,
+                    bookings__date_check_out__gt=checkin_date,
+                )
+                | Q(
+                    bookings__date_check_in__lt=checkout_date,
+                    bookings__date_check_out__gte=checkout_date,
+                )
+            )
+            # logger.debug(queryset)
+        elif checkin_date:
+            return Response(
+                data='You have to provide checkout_date', status=HTTPStatus.BAD_REQUEST
+            )
+        elif checkout_date:
+            return Response(
+                data='You have to provide checkin_date', status=HTTPStatus.BAD_REQUEST
+            )
+        else:
+            queryset = Room.objects.all()
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class BookingViewSet(viewsets.ModelViewSet):
-
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
@@ -27,22 +72,3 @@ class BookingViewSet(viewsets.ModelViewSet):
     pagination_class = None  # TODO which pagination?
     serializer_class = BookingSerializer
     permission_classes = [IsAdminOrCreate]
-
-    # @action(
-    #     detail=False,
-    #     methods=[
-    #         "get",
-    #         "delete",
-    #     ],
-    #     serializer_class=BookingSerializer,
-    #     permission_classes=[IsAuthor,]
-    # )
-    # def my_bookings(self, request,*args, **kwargs):
-    #     queryset = self.get_queryset()
-    #     page = self.paginate_queryset(queryset)
-    #     if page is not None:
-    #         serializer = self.get_serializer(page, many=True)
-    #         return self.get_paginated_response(serializer.data)
-    #
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     return Response(serializer.data)
