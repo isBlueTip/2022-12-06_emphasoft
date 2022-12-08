fbce0d46aec945842b649659b0aa610d659aa931
Simon Egorov <rhinoRofl@gmail.com>
2022-12-09 03:02:08 +0600

    Autoformat permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,37 +1,15 @@
-from rest_framework.permissions import (SAFE_METHODS, BasePermission,
-                                        IsAdminUser)
-
-
-class IsAuthenticatedOrReadOnlyOrRegister(BasePermission):
-    def has_permission(self, request, view):
-        return bool(
-            request.method == "POST"
-            or request.method == SAFE_METHODS
-            or request.user
-            and request.user.is_authenticated
-        )
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsAdminOrCreate(BasePermission):
     def has_permission(self, request, view):
         return bool(
-            (request.user and request.user.is_authenticated)
+            request.method == 'POST'
+            or (request.user and request.user.is_staff)
         )
 
 
-# class IsAuthor(BasePermission):
-#     def has_permission(self, request, view):
-#         print(1)
-#         return bool(
-#             # request.method == SAFE_METHODS
-#             request.user and request.user.is_authenticated
-#         )
-#
-#     def has_object_permission(self, request, view, obj):
-#         return bool(obj.guest == request.user)
-
-
-class IsAdminOrReadOnly(IsAdminUser):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.method in SAFE_METHODS
