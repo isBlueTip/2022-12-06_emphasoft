d17d3b70154e2d65518d079c0252199b6f5c5872
Simon Egorov <rhinoRofl@gmail.com>
2022-12-08 23:39:32 +0600

    Update default User model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,13 @@
+from django_paranoid.models import ParanoidModel
+
+# from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 from django.contrib.auth import get_user_model
+
+
+# class User(AbstractUser, ParanoidModel):
+#     field = models.CharField(max_length=20)
 
 User = get_user_model()
